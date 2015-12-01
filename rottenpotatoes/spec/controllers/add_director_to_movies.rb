bf9e75adfe0925e5b22c1e46b87209b8c37f28ab
Victor <victor.kam@gmail.com>
2015-12-01 09:34:19 +0000

    added files

---
+++ rottenpotatoes/spec/controllers/add_director_to_movies.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe MovieController do
+  describe 'update movie director name' do
+    it 'should find a '
+    it 'should call the model method that updates params in movie'
+  end
+end
